2049ce4af7614116241c4a3db3df336a1126f009
Eugene Gershnik <gershnik@users.noreply.github.com>
2025-06-29 00:33:56 -0700

    Updating wsddn to 1.19

--- Casks/wsddn.rb
+++ Casks/wsddn.rb
@@ -1,6 +1,6 @@
 cask "wsddn" do
-  version "1.18"
-  sha256 "eed58c6d57efac5c0aa967f9b8632dab91ce6f338962d36e6747051ac5255ae6"
+  version "1.19"
+  sha256 "0a467b1080934a0ce5ffcd3b14e0238ddac7392af5957a9e79c272a8e3ca15f5"
 
   url "https://github.com/gershnik/wsdd-native/releases/download/v#{version.major_minor}/wsddn-macos-#{version}.pkg" 
   name "WS-Discovery Host Daemon"
