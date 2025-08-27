7d94df2d8335b8cf8ee6e2a58bb9940a957bb385
Eugene Gershnik <gershnik@users.noreply.github.com>
2025-08-26 23:12:33 -0700

    Updating wsddn to 1.21-1 (fixing uninstall)

--- Casks/wsddn.rb
+++ Casks/wsddn.rb
@@ -1,8 +1,8 @@
 cask "wsddn" do
-  version "1.21"
+  version "1.21.1"
   sha256 "36f2df57110d0d940848e18a0614580a12c1dc3cfcf855366aefe4c2f6439e9f"
 
-  url "https://github.com/gershnik/wsdd-native/releases/download/v#{version.major_minor}/wsddn-macos-#{version}.pkg" 
+  url "https://github.com/gershnik/wsdd-native/releases/download/v#{version.major_minor}/wsddn-macos-#{version.major_minor}.pkg" 
   name "WS-Discovery Host Daemon"
   desc "Allows your Mac to be discovered by Windows 10 and above systems and displayed by their Explorer \"Network\" views."
   homepage "https://github.com/gershnik/wsdd-native"
@@ -15,12 +15,12 @@ cask "wsddn" do
   
   depends_on macos: ">= :catalina"
 
-  pkg "wsddn-macos-#{version}.pkg"
+  pkg "wsddn-macos-#{version.major_minor}.pkg"
 
   uninstall launchctl: "io.github.gershnik.wsddn",
             pkgutil: "io.github.gershnik.wsddn",
             script: {
-              executable:   "bash",
+              executable:   "/bin/bash",
               args:         ["-c", "dscl . -delete /Users/_wsddn; dscl . -delete /Groups/_wsddn; exit 0"],
               sudo:         true
             }
