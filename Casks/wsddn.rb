c3d690f8c745d0e88436426a9bcc98cd6ee97ecf
Eugene Gershnik <gershnik@users.noreply.github.com>
2026-06-22 23:34:53 -0700

    Updating wsddn OS dependency syntax

--- Casks/wsddn.rb
+++ Casks/wsddn.rb
@@ -2,28 +2,28 @@ cask "wsddn" do
   version "1.24"
   sha256 "6f9ed624475b3e6d44c23243d016fd4f54a520590a320987d016f1ef03152863"
 
-  url "https://github.com/gershnik/wsdd-native/releases/download/v#{version.major_minor}/wsddn-macos-#{version.major_minor}.pkg" 
+  url "https://github.com/gershnik/wsdd-native/releases/download/v#{version.major_minor}/wsddn-macos-#{version.major_minor}.pkg"
   name "WS-Discovery Host Daemon"
-  desc "Allows your Mac to be discovered by Windows 10 and above systems and displayed by their Explorer \"Network\" views."
+  desc "Allows your Mac to be discovered by Windows 10 and above systems and displayed by their Explorer \"Network\" views"
   homepage "https://github.com/gershnik/wsdd-native"
-  
+
   livecheck do
     url "https://github.com/gershnik/wsdd-native/releases.atom"
-    regex(%r{\<id\>tag:github.com,2008:Repository/[0-9]+/v([0-9.]+)\</id\>}i)
+    regex(%r{<id>tag:github\.com,2008:Repository/[0-9]+/v([0-9.]+)</id>}i)
     strategy :page_match
   end
-  
-  depends_on macos: ">= :catalina"
+
+  depends_on macos: :catalina
 
   pkg "wsddn-macos-#{version.major_minor}.pkg"
 
   uninstall launchctl: "io.github.gershnik.wsddn",
-            pkgutil: "io.github.gershnik.wsddn",
-            script: {
-              executable:   "/bin/bash",
-              args:         ["-c", "dscl . -delete /Users/_wsddn; dscl . -delete /Groups/_wsddn; exit 0"],
-              sudo:         true
-            }
-            
+            script:    {
+              executable: "/bin/bash",
+              args:       ["-c", "dscl . -delete /Users/_wsddn; dscl . -delete /Groups/_wsddn; exit 0"],
+              sudo:       true,
+            },
+            pkgutil:   "io.github.gershnik.wsddn"
+
   zap delete: "/etc/wsddn.conf"
 end
